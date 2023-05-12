e9c9aacbcf72d3fc0ab7a8695fc8ecf98fe7d622
Demetrius <Dmayfield@appdev.yearup.or>
2023-05-12 11:03:50 -0400

    first commit

---
+++ src/main/java/com/dm/Program.java
@@ -0,0 +1,14 @@
+package com.dm;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Program {
+
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<Vehicle> vehicles = new ArrayList<>();
+    public static void main(String[] args) {
+
+
+    }
+}
